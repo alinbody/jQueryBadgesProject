59fbff7e82bde3b52ebc9cc8bcd630f4d5ec0083
Alin Ionescu <alinionescu@MBP-di-Alin.homenet.telecomitalia.it>
2017-01-10 19:50:23 +0100

    finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
+$.ajax({
+  url: 'https://www.codeschool.com/users/alinbody.json',
+  dataType:'jsonp',
+  success: function(response){
+    // all work there
+    addCourses(response.courses.completed);
 
-  // your code will go here
+  }//closing success
+});//closing $.ajax
+
+function addCourses(courses){
+  var $badges = $('#badges');
+
+  courses.forEach(function(course){
+
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />',{
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: "See Course"
+    }).appendTo($course);
+
+  });
+
+}
+
 
 });
